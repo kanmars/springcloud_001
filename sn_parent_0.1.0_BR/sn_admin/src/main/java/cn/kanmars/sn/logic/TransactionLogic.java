6cd6bc2c34cdd6ba79f5c9ee1219f0a288a174b3
kanmars <x_wbs@aliyun.com>
2016-12-23 17:44:12 +0800

    n

--- sn_parent_0.1.0_BR/sn_admin/src/main/java/cn/kanmars/sn/logic/TransactionLogic.java
+++
@@ -1,8 +0,0 @@
-package cn.kanmars.sn.logic;
-
-/**
- * Created by baolong on 2016/1/12.
- */
-public interface TransactionLogic {
-    public void exec() throws Exception;
-}
--- sn_parent_0.1.0_BR/sn_generator/src/main/java/pipe/impl/meta/SQLTYPE.java
+++
@@ -1,8 +0,0 @@
-package pipe.impl.meta;
-
-/**
- * Created by baolong on 2016/8/23.
- */
-public enum SQLTYPE {
-    INT,BIGINT,NUMBER,CHAR,VARCHAR,TINYBLOB,BLOB,MEDIUMBLOB,LONGBLOB
-}
